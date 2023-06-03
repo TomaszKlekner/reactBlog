051deb3fa9656f255ae8fc0ae612c8df038e0bdc
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-03 19:48:09 +0200

    Add link to post in post list

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -1,24 +1,33 @@
+import { Link } from "react-router-dom";
 import "./post.scss";
 import postImage from "../../assets/sidebar_about_me.jpg";
 
-const Post = () => {
+interface Props {
+  post: {
+    id: number;
+  };
+}
+
+const Post = ({ post }: Props) => {
   return (
     <div className="post">
-      <img className="post__image" src={postImage} alt="Post Title" />
-      <div className="post__info">
-        <div className="post__categories">
-          <span className="post__category">Music</span>
-          <span className="post__category">Life</span>
+      <Link className="post__link" to={`posts/${post.id}`}>
+        <img className="post__image" src={postImage} alt="Post Title" />
+        <div className="post__info">
+          <div className="post__categories">
+            <span className="post__category">Music</span>
+            <span className="post__category">Life</span>
+          </div>
+          <h3 className="post__title">Lorem ipsum dolor sit amet.</h3>
+          <hr />
+          <span className="post__date">1 hour ago</span>
         </div>
-        <h3 className="post__title">Lorem ipsum dolor sit amet.</h3>
-        <hr />
-        <span className="post__date">1 hour ago</span>
-      </div>
-      <p className="post_description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        corrupti commodi debitis laudantium iusto accusamus unde voluptates
-        nobis. Minima, accusamus?
-      </p>
+        <p className="post_description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+          corrupti commodi debitis laudantium iusto accusamus unde voluptates
+          nobis. Minima, accusamus?
+        </p>
+      </Link>
     </div>
   );
 };
--- client/src/components/posts/Posts.tsx
+++ client/src/components/posts/Posts.tsx
@@ -5,15 +5,15 @@ const Posts = () => {
   return (
     <div className="posts">
       <div className="posts__grid grid__container">
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post post={{ id: 1 }} />
+        <Post post={{ id: 2 }} />
+        <Post post={{ id: 3 }} />
+        <Post post={{ id: 4 }} />
+        <Post post={{ id: 5 }} />
+        <Post post={{ id: 6 }} />
+        <Post post={{ id: 7 }} />
+        <Post post={{ id: 8 }} />
+        <Post post={{ id: 9 }} />
       </div>
     </div>
   );
