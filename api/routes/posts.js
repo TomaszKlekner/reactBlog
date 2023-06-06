6e735281f0a4590bc9fb8f30c3bd14e82ec245d7
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-06 14:18:07 +0200

    Remove unused imports

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,6 @@
 import express from "express";
-import bcrypt from "bcrypt";
-import { UserModel } from "../model/User.js";
 import { PostModel } from "../model/Post.js";
 const router = express.Router();
-const saltRounds = 10;
 
 // Create post
 router.post("/", async (req, res) => {
