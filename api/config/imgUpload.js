ddd1b67fbe5e8ea7874309d83b2d4c31389a4258
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-07 08:59:58 +0200

    Create image upload with multer

---
+++ api/config/imgUpload.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+export { upload };
