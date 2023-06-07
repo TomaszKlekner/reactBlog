42619bf996bca5efc013868e8cb4112e282b23c0
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-07 10:12:10 +0200

    Display single post data

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -1,35 +1,42 @@
 import { Link } from "react-router-dom";
+import moment from "moment";
 import "./post.scss";
 import postImage from "../../assets/sidebar_about_me.jpg";
+import { IPost } from "../../shared/post.model";
 
 interface Props {
-  post: {
-    id: number;
-  };
+  post: IPost;
 }
 
 const Post = ({ post }: Props) => {
+  const { title, description, categories, createdAt } = post;
+  const createdAtAgo = moment
+    .utc(createdAt)
+    .local()
+    .startOf("seconds")
+    .fromNow();
+
+  console.log(post);
   return (
     <div className="post">
-      <img className="post__image" src={postImage} alt="Post Title" />
+      <img className="post__image" src={postImage} alt={title} />
       <div className="post__info">
         <div className="post__categories">
-          <span className="post__category">Music</span>
-          <span className="post__category">Life</span>
+          {categories.map((category) => (
+            <span key={category} className="post__category">
+              {category}
+            </span>
+          ))}
         </div>
         <h3 className="post__title">
-          <Link className="post__link" to={`posts/${post.id}`}>
-            Lorem ipsum dolor sit amet.
+          <Link className="post__link" to={`posts/${title}`}>
+            {title}
           </Link>
         </h3>
         <hr />
-        <span className="post__date">1 hour ago</span>
+        <span className="post__date">{createdAtAgo}</span>
       </div>
-      <p className="post_description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-        corrupti commodi debitis laudantium iusto accusamus unde voluptates
-        nobis. Minima, accusamus?
-      </p>
+      <p className="post_description">{description}</p>
     </div>
   );
 };
