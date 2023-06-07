bdf18ee48e861c0c280f28f7527e75d930112ced
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-07 09:46:19 +0200

    Install cors package

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import morgan from "morgan";
 import helmet from "helmet";
 import mongoose from "mongoose";
+import cors from "cors";
 import connectDb from "./config/dbConnect.js";
 import { upload } from "./config/imgUpload.js";
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Use middleware
+app.use(cors());
 app.use(morgan("common"));
 app.use(helmet());
 app.use(express.json());
