5218be8c60e6cd6fe8a6b3d593fd7b244b05bab8
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-07 10:11:29 +0200

    Fetch posts and pass as props

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "../../axios";
+
 import Hero from "../../components/hero/Hero";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
+import { IPost } from "../../shared/post.model";
 import "./home.scss";
 
 const Home = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get("posts");
+      setPosts(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <Hero />
       <main className="home container">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </main>
     </>
