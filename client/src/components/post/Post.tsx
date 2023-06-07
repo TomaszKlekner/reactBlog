48319d808fb8dcd0589ceceab815e0e92571e655
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-07 17:08:15 +0200

    Update post component do display data

--- client/src/components/post/Post.tsx
+++ client/src/components/post/Post.tsx
@@ -1,40 +1,36 @@
 import { Link } from "react-router-dom";
-import moment from "moment";
-import "./post.scss";
-import postImage from "../../assets/sidebar_about_me.jpg";
 import { IPost } from "../../shared/post.model";
+import "./post.scss";
 
 interface Props {
   post: IPost;
 }
 
 const Post = ({ post }: Props) => {
-  const { title, description, categories, createdAt } = post;
-  const createdAtAgo = moment
-    .utc(createdAt)
-    .local()
-    .startOf("seconds")
-    .fromNow();
+  const { _id, title, description, photo, categories, createdAt } = post;
 
-  console.log(post);
   return (
     <div className="post">
-      <img className="post__image" src={postImage} alt={title} />
+      {photo && <img className="post__image" src={photo} alt={title} />}
       <div className="post__info">
         <div className="post__categories">
           {categories.map((category) => (
-            <span key={category} className="post__category">
+            <Link
+              key={category}
+              className="post__category"
+              to={`posts/?category=${category}`}
+            >
               {category}
-            </span>
+            </Link>
           ))}
         </div>
         <h3 className="post__title">
-          <Link className="post__link" to={`posts/${title}`}>
+          <Link className="post__link" to={`posts/${_id}`}>
             {title}
           </Link>
         </h3>
         <hr />
-        <span className="post__date">{createdAtAgo}</span>
+        <span className="post__date">{new Date(createdAt).toDateString()}</span>
       </div>
       <p className="post_description">{description}</p>
     </div>
