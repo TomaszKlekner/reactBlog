77e4b3067ba12f30ac3ac550b51832bdfde1fe0b
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-15 18:31:19 +0200

    Extract user actions to a separate file

---
+++ client/src/context/UserActions.tsx
@@ -0,0 +1,34 @@
+import CUser from "../shared/user.model";
+
+export enum UserActionType {
+  LOGIN_START = "LOGIN_START",
+  LOGIN_SUCCESS = "LOGIN_SUCCESS",
+  LOGIN_ERROR = "LOGIN_ERROR",
+  LOGOUT = "LOGOUT",
+}
+
+type UserActionLogin = {
+  type: UserActionType.LOGIN_START;
+  payload: null;
+};
+
+type UserActionSuccess = {
+  type: UserActionType.LOGIN_SUCCESS;
+  payload: CUser;
+};
+
+type UserActionError = {
+  type: UserActionType.LOGIN_ERROR;
+  payload: null;
+};
+
+type UserActionLogout = {
+  type: UserActionType.LOGOUT;
+  payload: null;
+};
+
+export type UserAction =
+  | UserActionLogin
+  | UserActionSuccess
+  | UserActionError
+  | UserActionLogout;
