a23a46ea3ae8e377da9f2fcc74456ece38bf51d6
Tomasz Klekner <tomasz.klekner@gmail.com>
2023-06-20 08:51:56 +0200

    Create add post functionality

--- client/src/pages/addPost/AddPost.tsx
+++ client/src/pages/addPost/AddPost.tsx
@@ -1,22 +1,80 @@
 import { FaPlus } from "react-icons/fa";
-import heroBg from "../../assets/hero_bg.jpg";
 import "./add-post.scss";
-import CUser from "../../shared/user.model";
+import { ChangeEvent, useContext, useState } from "react";
+import axios from "../../axios";
+import { IPost } from "../../shared/post.model";
+import { UserContext } from "../../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 const Write = () => {
+  const {
+    state: { user },
+  } = useContext(UserContext);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [file, setFile] = useState<File | null | undefined>(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (user) {
+      const newPost: IPost = {
+        author: user?.username,
+        title,
+        description,
+        isPublished: false,
+      };
+
+      if (file) {
+        const data = new FormData();
+        const filename = Date.now() + file.name;
+        data.append("name", filename);
+        data.append("file", file);
+        newPost.photo = filename;
+
+        try {
+          await axios.post<IPost>("/upload", data);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
+      try {
+        const { data } = await axios.post<IPost>("/posts", newPost);
+        navigate(`/posts/${data._id}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="add-post container">
-      <img src={heroBg} alt="" className="add-post__image" />
+      {file && (
+        <img
+          src={URL.createObjectURL(file)}
+          alt=""
+          className="add-post__image"
+        />
+      )}
 
-      <form className="form add-post-form">
+      <form className="form add-post-form" onSubmit={handleSubmit}>
         <div className="form__group add-post-form__group">
-          <label htmlFor="file__input" className="add-post-form__label">
+          <label
+            htmlFor="file__input"
+            className="add-post-form__label"
+            title="Add Post Image"
+          >
             <FaPlus />
           </label>
           <input
             type="file"
             id="file__input"
             className="file__input file__input--file add-post-form__input--file"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setFile(e.target.files?.[0])
+            }
           />
           <input
             type="text"
@@ -24,6 +82,10 @@ const Write = () => {
             placeholder="Title"
             autoFocus={true}
             required
+            value={title}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setTitle(e.target.value)
+            }
           />
         </div>
 
@@ -34,6 +96,10 @@ const Write = () => {
             rows={10}
             placeholder="Tell your story"
             required
+            value={description}
+            onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+              setDescription(e.target.value)
+            }
           ></textarea>
         </div>
 
